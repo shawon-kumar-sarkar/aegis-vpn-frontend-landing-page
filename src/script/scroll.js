946000653e2f200bf0b8e5d02c8dbb32ab108869
shawon-kumar-sarkar <shawonkumarsarkar.developer@gmail.com>
2025-09-28 15:24:30 +0600

    Lenis added

---
+++ src/script/scroll.js
@@ -0,0 +1,12 @@
+"use strict";
+// eslint-disable-next-line no-undef
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: t => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smooth: true,
+});
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
